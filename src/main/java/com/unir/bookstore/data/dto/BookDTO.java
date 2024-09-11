427fd2b61fa181c237d98e6dd6c9eaf7dc8093ed
victor0604 <37073426+victor0604@users.noreply.github.com>
2024-09-11 10:38:23 -0500

    Add files via upload

--- src/main/java/com/unir/bookstore/data/dto/BookDTO.java
+++ src/main/java/com/unir/bookstore/data/dto/BookDTO.java
@@ -1,1 +1,18 @@
+package com.unir.bookstore.data.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private Double price;
+}
