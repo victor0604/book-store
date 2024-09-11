1db92f841f4ce651b9f6905b22c06d129ac99a4d
victor0604 <37073426+victor0604@users.noreply.github.com>
2024-09-11 10:40:43 -0500

    Add files via upload

--- src/main/java/com/unir/bookstore/data/repository/BookRepository.java
+++ src/main/java/com/unir/bookstore/data/repository/BookRepository.java
@@ -1,1 +1,10 @@
+package com.unir.bookstore.data.repository;
 
+import com.unir.bookstore.data.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+}
